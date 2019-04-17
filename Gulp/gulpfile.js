9d9828a85ef5700cf7c758737063a99e5e3a9373
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2019-04-17 13:01:50 +0800

    gulpfiless

---
+++ Gulp/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+
+gulp.task('jshint', function(){
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+})
